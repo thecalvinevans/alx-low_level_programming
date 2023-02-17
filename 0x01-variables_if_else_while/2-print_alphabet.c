9312b99c49e456f41771df9b1e88d990d82f4c57
thecalvinevans <thecalvinevans@gmail.com>
2023-02-17 03:17:30 -0800

    The last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Get a random  nunber and check its last digits, compare it with 5
+ * Return 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
