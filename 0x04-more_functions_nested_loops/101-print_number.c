ec1de52a4db885a228f37e7450c7e798f64b398f
thecalvinevans <thecalvinevans@gmail.com>
2023-02-23 09:37:03 -0800

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints an integer
+ * @n: integer to be printe
+ * Return: Always 0 (success)
+ */
+void print_number(int n)
+{
+unsigned int n1;
+
+if (n < 0)
+{
+n1 = n;
+_putchar('_');
+}
+else
+{
+n1 = n;
+}
+
+if (n1 / 10)
+{
+print_number(n1 / 10);
+}
+
+_putchar((n1 % 10) + '0');
+
+}
+
+
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,39 +2,37 @@
 #include <stdio.h>
 
 /**
- * main - print the numbers from 1 to 100, followed by a new line
- * but for  multiples of three prints FIZZ instead of the number
- * and for multiples of five prints BUZZ
+ * main - prints a FIZZ BUZZ program
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
-int i;
+	int x;
 
-for (i = 1; i <= 100; i++)
-{
-if (i % 3 == 0 && i % 5 != 0)
-{
-pintf("FIZZ");
-}
-else if (i % 5 == 0 && i % 3 != 0)
-{
-printf("BUZZ");
-}
-else if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FIZZBUZZ");
-}
-else if (i == 1)
-{
-printf("%d", i);
-}
-else
-{
-printf("%d", i);
-}
-}
-printf("\n");
-
-return (0);
+	for (x = 1; x <= 100; x++)
+	{
+	if ((x % 3 == 0) && (x % 5 == 0))
+	{
+	printf("FIZZBUZZ");
+	}
+	else if (x % 3 == 0)
+	{
+	printf("FIZZ");
+	}
+	else if (x % 5 == 0)
+	{
+	printf("BUZZ");
+	}
+	else
+	{
+	printf("%d", x);
+	}
+	if (x != 100)
+	{
+	printf(" ");
+	}
+	}
+	printf("\n");
+	return (0);
 }
